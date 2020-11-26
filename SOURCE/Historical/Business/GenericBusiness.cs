91952b744a3bdc7517380f5d242b13878614bd50
BuiThanh-HB <thanhstone98@gmail.com>
2020-11-26 19:23:27 +0700

    Update

---
+++ SOURCE/Historical/Business/GenericBusiness.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Historical.Models;
+
+namespace Historical.Business
+{
+    public class GenericBusiness
+    {
+        public HistoricalContext context;
+        public HistoricalContext cnn;
+        public GenericBusiness(HistoricalContext context = null)
+        {
+            if (context == null)
+            {
+                this.context = new HistoricalContext();
+            }
+            cnn = this.context;
+        }
+
+    }
+}
