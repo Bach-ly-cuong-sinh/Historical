7bc18222059117e7b60d5c076c76060727736808
An <nguyenminhan696@gmail.com>
2020-12-04 22:50:26 +0700

    update contact

---
+++ SOURCE/Historical/Business/ContactUsBusiness.cs
@@ -0,0 +1,31 @@
+﻿using Historical.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Historical.Business
+{
+    public class ContactUsBusiness:GenericBusiness
+    {
+        public int Create(string Mes, string Name, string Email)
+        {
+            try
+            {
+                Contact c = new Contact();
+                c.Content = Mes;
+                c.Email = Email;
+                c.Name = Name;
+                c.IsActive = 1;
+                c.CreatedDate = DateTime.Now;
+                cnn.Contacts.Add(c);
+                cnn.SaveChanges();
+                return c.Id;
+            }catch(Exception e)
+            {
+                e.ToString();
+                return 0;
+            }
+        }
+    }
+}
--- SOURCE/Historical/Controllers/ContactUsController.cs
+++ SOURCE/Historical/Controllers/ContactUsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Historical.Business;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,26 @@ namespace Historical.Controllers
 {
     public class ContactUsController : Controller
     {
+        ContactUsBusiness contactUsBusiness = new ContactUsBusiness();
         public IActionResult Index()
         {
             return View();
+        }
+        public int Create( string Mes, string Name,string Email)
+        {
+            try
+            {
+                var data = contactUsBusiness.Create(Mes,Name,Email);
+                if(data > 0)
+                {
+                    return data;
+                }
+                return 0;
+            }catch(Exception e)
+            {
+                e.ToString();
+                return 0;
+            }
         }
     }
 }
