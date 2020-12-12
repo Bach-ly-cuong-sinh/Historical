b97672049be3c54dcbc70d4ec41101df20fe8e79
An <nguyenminhan696@gmail.com>
2020-12-12 18:19:47 +0700

    update relics

---
+++ SOURCE/Historical/Business/RelicsBusiness.cs
@@ -0,0 +1,43 @@
+﻿using Historical.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Historical.Business
+{
+    public class RelicsBusiness:GenericBusiness
+    {
+        public List<RelicsOutputModel> ListRelic()
+        {
+            try
+            {
+                List<RelicsOutputModel> data = new List<RelicsOutputModel>();
+                var q = from r in cnn.Relics
+                        where r.IsActive.Equals(1)
+                        select (new RelicsOutputModel()
+                        {
+                            CategoryID = r.Cate.Id,
+                            CategoryName = r.Cate.Name,
+                            Address = r.Address,
+                            CreatedDate = r.CraetedDate.Value,
+                            ImageUrl = r.ImageUrl,
+                            MapUrl = r.MapLinks,
+                            RelicsName = r.Name,
+                            RelicsID = r.Id
+                        });
+                if (q != null)
+                {
+                    data = q.ToList();
+                    return data;
+                }
+
+                return new List<RelicsOutputModel>();
+            }
+            catch
+            {
+                return new List<RelicsOutputModel>();
+            }
+        }
+    }
+}
